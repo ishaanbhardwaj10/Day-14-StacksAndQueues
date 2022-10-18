675bd257454d6cbaedb2706d9e4d539e0bea464d
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-18 14:41:59 +0530

    added UC-2 option to switch statement

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
 
             Console.WriteLine("Welcome to Stacks and Queues Problem");
             Console.WriteLine("Use case #1: Create a Stack");
+            Console.WriteLine("Use case #2: Peek and Pop from Stack till its empty");
 
             Console.Write("Please select a program to run from options above: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -20,6 +21,20 @@
                     stack.Push(30);
                     stack.Push(56);
                     stack.Display();
+                    break;
+                case 2:
+                    Stack stackOne = new Stack();
+                    stackOne.Push(70);
+                    stackOne.Push(30);
+                    stackOne.Push(56);
+                    Console.WriteLine("-----------Stack before empty operation-----------");
+                    stackOne.Display();
+                    Console.WriteLine("\n--------------------------------------------------");
+                    Console.Write("\n");
+                    stackOne.IsEmpty();
+                    Console.WriteLine("-----------Stack after empty operation-----------");
+                    stackOne.Display();
+                    Console.WriteLine("-------------------------------------------------");
                     break;
                 default:
                     Console.WriteLine("Please enter a valid number from given options");
