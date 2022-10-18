a04088b75d30f27acd9522ac4f5d7d7b05839fe6
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-18 15:51:57 +0530

    added UC-4 option to switch statement

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@
             Console.WriteLine("Use case #1: Create a Stack");
             Console.WriteLine("Use case #2: Peek and Pop from Stack till its empty");
             Console.WriteLine("Use case #3: Create a Queue");
+            Console.WriteLine("Use case #4: Dequeue from beginning of Queue");
 
             Console.Write("Please select a program to run from options above: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -43,6 +44,19 @@
                     queueOne.Enqueue(30);
                     queueOne.Enqueue(70);
                     queueOne.Display();
+                    break;
+                case 4:
+                    Queue queueTwo = new Queue();
+                    queueTwo.Enqueue(56);
+                    queueTwo.Enqueue(30);
+                    queueTwo.Enqueue(70);
+                    Console.WriteLine("-----------Queue before dequeue operation-----------");
+                    queueTwo.Display();
+                    Console.WriteLine("\n--------------------------------------------------");
+                    queueTwo.Dequeue();
+                    Console.WriteLine("-----------Queue after dequeue operation-----------");
+                    queueTwo.Display();
+                    Console.WriteLine("\n--------------------------------------------------");
                     break;
                 default:
                     Console.WriteLine("Please enter a valid number from given options");
